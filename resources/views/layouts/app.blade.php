6289254e2e095117c71edd5e75a0103e7683573f
Mustafa Syahmi <mussyahmi31@gmail.com>
2024-02-21 22:15:12 +0800

    Add sidebar & avatar and logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,12 +32,8 @@
         </div>
         <div class="drawer-side">
             <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
-            <ul class="min-h-full p-4 menu w-80 bg-base-200 text-base-content">
-                <!-- Sidebar content here -->
-                <li><a>Sidebar Item 1</a></li>
-                <li><a>Sidebar Item 2</a></li>
-            </ul>
 
+            @include('layouts.sidebar')
         </div>
     </div>
 </body>
