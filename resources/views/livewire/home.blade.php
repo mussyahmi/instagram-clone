bb7f8e45fb4e68036ec4c0cc6fcf5d11d26cdff1
Mustafa Syahmi <mussyahmi31@gmail.com>
2024-02-22 09:34:04 +0800

    Design story section

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -4,6 +4,18 @@
 
     <main class="grid gap-8 lg:grid-cols-12 md:mt-10">
         <aside class="lg:col-span-8 border overflow-hidden h-[1000px]">
+            {{-- stories --}}
+            <section>
+                <ul class="flex items-center gap-2 overflow-x-auto">
+                    @for ($i = 0; $i < 20; $i++)
+                        <li class="flex flex-col justify-center w-20 gap-1 p-2">
+                            <x-avatar src="https://source.unsplash.com/500x500?face-{{ $i }}" class="h-14 w-14" />
+
+                            <p class="text-xs font-medium truncate">{{ fake()->name }}</p>
+                        </li>
+                    @endfor
+                </ul>
+            </section>
         </aside>
 
         {{-- suggestions --}}
