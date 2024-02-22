b9a5f84098558d4ebd1a0059f15dc5d9d90c3d1e
Mustafa Syahmi <mussyahmi31@gmail.com>
2024-02-22 09:10:02 +0800

    Add home component

---
+++ resources/views/livewire/home.blade.php
@@ -0,0 +1,13 @@
+<div class="w-full">
+    <header>
+    </header>
+
+    <main class="grid gap-8 lg:grid-cols-12 md:mt-10">
+        <aside class="lg:col-span-8 border overflow-hidden h-[1000px]">
+        </aside>
+
+        {{-- suggestions --}}
+        <aside class="hidden p-4 border lg:col-span-4 lg:block">
+        </aside>
+    </main>
+</div>
